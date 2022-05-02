84cc88bf08c6d573fcced1892097c1ccdf41efd0
Renan Campos <rcampos@redhat.com>
2022-05-02 13:06:13 -0400

    improvements to byo-vpc terraform scripts
    - Region independent (will use w/e region aws is configure to)
    - Dynamic number of subnets
    - Spread subnets across available availability zones in region

--- terraform/vpc-resources.tf
+++ terraform/vpc-resources.tf
@@ -9,62 +9,31 @@ resource "aws_vpc" "site" {
 
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.site.id
-  service_name = "com.amazonaws.${var.region}.s3"
+  service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
 }
 
-resource "aws_subnet" "public1" {
+resource "aws_subnet" "public" {
+  count = var.subnet_count
+
   vpc_id            = aws_vpc.site.id
-  cidr_block        = "10.0.0.0/20"
-  availability_zone = "${var.region}a"
+  cidr_block        = cidrsubnet("10.0.0.0/16", var.subnet_count * 2, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
   tags = {
-    Name = join("-", [var.site_name, "subnet", "public1", "${var.region}a"])
+    Name = join("-", [var.site_name, "subnet", "public${count.index + 1}", data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]])
   }
 }
 
-resource "aws_subnet" "public2" {
+resource "aws_subnet" "private" {
+  count = var.subnet_count
+
   vpc_id            = aws_vpc.site.id
-  cidr_block        = "10.0.16.0/20"
-  availability_zone = "${var.region}b"
+  cidr_block        = cidrsubnet("10.0.0.0/16", var.subnet_count * 2, count.index + var.subnet_count)
+  availability_zone = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
   tags = {
-    Name = join("-", [var.site_name, "subnet", "public2", "${var.region}b"])
+    Name = join("-", [var.site_name, "subnet", "private${count.index + 1}", data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]])
   }
 }
 
-resource "aws_subnet" "public3" {
-  vpc_id            = aws_vpc.site.id
-  cidr_block        = "10.0.32.0/20"
-  availability_zone = "${var.region}c"
-  tags = {
-    Name = join("-", [var.site_name, "subnet", "public3", "${var.region}c"])
-  }
-}
-
-resource "aws_subnet" "private1" {
-  vpc_id            = aws_vpc.site.id
-  cidr_block        = "10.0.128.0/20"
-  availability_zone = "${var.region}a"
-  tags = {
-    Name = join("-", [var.site_name, "subnet", "private1", "${var.region}a"])
-  }
-}
-
-resource "aws_subnet" "private2" {
-  vpc_id            = aws_vpc.site.id
-  cidr_block        = "10.0.144.0/20"
-  availability_zone = "${var.region}b"
-  tags = {
-    Name = join("-", [var.site_name, "subnet", "private2", "${var.region}b"])
-  }
-}
-
-resource "aws_subnet" "private3" {
-  vpc_id            = aws_vpc.site.id
-  cidr_block        = "10.0.160.0/20"
-  availability_zone = "${var.region}c"
-  tags = {
-    Name = join("-", [var.site_name, "subnet", "private3", "${var.region}c"])
-  }
-}
 
 #
 # Internet gateway
@@ -79,54 +48,27 @@ resource "aws_internet_gateway" "site" {
 #
 # Elastic IPs for NAT gateways
 #
-resource "aws_eip" "nat1" {
+resource "aws_eip" "nat" {
+  count = var.subnet_count
+
   vpc = true
   tags = {
-    Name = join("-", [var.site_name, "eip", "${var.region}a"])
+    Name = join("-", [var.site_name, "eip", data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]])
   }
 }
 
-resource "aws_eip" "nat2" {
-  vpc = true
-  tags = {
-    Name = join("-", [var.site_name, "eip", "${var.region}b"])
-  }
-}
-
-resource "aws_eip" "nat3" {
-  vpc = true
-  tags = {
-    Name = join("-", [var.site_name, "eip", "${var.region}c"])
-  }
-}
 
 #
 # NAT gateways
 #
-resource "aws_nat_gateway" "public1" {
-  allocation_id = aws_eip.nat1.id
-  subnet_id     = aws_subnet.public1.id
+resource "aws_nat_gateway" "public" {
+  count = var.subnet_count
+
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
 
   tags = {
-    Name = join("-", [var.site_name, "nat", "public1", "${var.region}a"])
-  }
-}
-
-resource "aws_nat_gateway" "public2" {
-  allocation_id = aws_eip.nat2.id
-  subnet_id     = aws_subnet.public2.id
-
-  tags = {
-    Name = join("-", [var.site_name, "nat", "public2", "${var.region}b"])
-  }
-}
-
-resource "aws_nat_gateway" "public3" {
-  allocation_id = aws_eip.nat3.id
-  subnet_id     = aws_subnet.public3.id
-
-  tags = {
-    Name = join("-", [var.site_name, "nat", "public3", "${var.region}c"])
+    Name = join("-", [var.site_name, "nat", "public${count.index}", data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]])
   }
 }
 
@@ -140,24 +82,12 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table" "private1" {
-  vpc_id = aws_vpc.site.id
-  tags = {
-    Name = join("-", [var.site_name, "rtb", "private1", "${var.region}a"])
-  }
-}
+resource "aws_route_table" "private" {
+  count = var.subnet_count
 
-resource "aws_route_table" "private2" {
   vpc_id = aws_vpc.site.id
   tags = {
-    Name = join("-", [var.site_name, "rtb", "private2", "${var.region}b"])
-  }
-}
-
-resource "aws_route_table" "private3" {
-  vpc_id = aws_vpc.site.id
-  tags = {
-    Name = join("-", [var.site_name, "rtb", "private3", "${var.region}c"])
+    Name = join("-", [var.site_name, "rtb", "private${count.index}", data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]])
   }
 }
 
@@ -181,72 +111,37 @@ resource "aws_route" "ipv6_egress_route" {
 }
 
 # Send private traffic to NAT
-resource "aws_route" "private1_nat" {
-  route_table_id         = aws_route_table.private1.id
+resource "aws_route" "private_nat" {
+  count = var.subnet_count
+
+  route_table_id         = aws_route_table.private[count.index].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.public1.id
-  depends_on             = [aws_route_table.private1, aws_nat_gateway.public1]
+  nat_gateway_id         = aws_nat_gateway.public[count.index].id
+  depends_on             = [aws_route_table.private, aws_nat_gateway.public]
 }
 
-resource "aws_route" "private2_nat" {
-  route_table_id         = aws_route_table.private2.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.public2.id
-  depends_on             = [aws_route_table.private2, aws_nat_gateway.public2]
-}
-
-resource "aws_route" "private3_nat" {
-  route_table_id         = aws_route_table.private3.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.public3.id
-  depends_on             = [aws_route_table.private3, aws_nat_gateway.public3]
-}
 
 # Private route for vpc endpoint
-resource "aws_vpc_endpoint_route_table_association" "private1" {
-  route_table_id  = aws_route_table.private1.id
-  vpc_endpoint_id = aws_vpc_endpoint.s3.id
-}
+resource "aws_vpc_endpoint_route_table_association" "private" {
+  count = var.subnet_count
 
-resource "aws_vpc_endpoint_route_table_association" "private2" {
-  route_table_id  = aws_route_table.private2.id
-  vpc_endpoint_id = aws_vpc_endpoint.s3.id
-}
-
-resource "aws_vpc_endpoint_route_table_association" "private3" {
-  route_table_id  = aws_route_table.private3.id
+  route_table_id  = aws_route_table.private[count.index].id
   vpc_endpoint_id = aws_vpc_endpoint.s3.id
 }
 
 #
 # Route table associations
 #
-resource "aws_route_table_association" "public1" {
-  subnet_id      = aws_subnet.public1.id
+resource "aws_route_table_association" "public" {
+  count = var.subnet_count
+
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "public2" {
-  subnet_id      = aws_subnet.public2.id
-  route_table_id = aws_route_table.public.id
-}
+resource "aws_route_table_association" "private" {
+  count = var.subnet_count
 
-resource "aws_route_table_association" "public3" {
-  subnet_id      = aws_subnet.public3.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "private1" {
-  subnet_id      = aws_subnet.private1.id
-  route_table_id = aws_route_table.private1.id
-}
-
-resource "aws_route_table_association" "private2" {
-  subnet_id      = aws_subnet.private2.id
-  route_table_id = aws_route_table.private2.id
-}
-
-resource "aws_route_table_association" "private3" {
-  subnet_id      = aws_subnet.private3.id
-  route_table_id = aws_route_table.private3.id
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
